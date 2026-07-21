d05cfeb940b5400518229baaa7d2e8b797acf5d4
anteliaofficial <anteliaofficial@gmail.com>
2026-07-21 08:54:18 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antelia-shell-v90';
+const CACHE = 'antelia-shell-v91';
 const SHELL = ['./index.html', './manifest.json'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
